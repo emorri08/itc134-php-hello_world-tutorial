4f8f503a0538434044fce445c4d44b64eecec8f5
Elly Boyd <emorri08@seattlecentral.edu>
2019-03-04 00:36:59 +0000

    adds index.php and main.css

---
+++ index.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="css/main.css" />
+<?php
+    
+    echo "
+        <p>Hello!</p>
+        <p>This is a basic PHP program.</p>
+        <p>It prints a few lines of text to the screen.</p>
+        <p>Abby, Salsa, and Gaucho are cats.</p>
+        <p>Gaucho, Abby, and Salsa are tabby cats.</p>
+        <p>=^. .^=</p>
+        <p>=^. .^=</p>
+        <p>=^. .^=</p>
+        <p>Cats are great!</p>
+        <p>That is all.</p>
+        <p>Have a nice day.</p>
+    ";
+
+
+
+
+
+
+
+
+
+
+
+?>
