df665d5444121d8416291e80eae53222240661a2
Elly Boyd <emorri08@seattlecentral.edu>
2019-03-16 17:19:26 +0000

    adds hello-world-hist.php and report.css

---
+++ hello-world-hist.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>The History of the Hello World Program | Nicole Finamore | ITC134 | Winter 2019 | A6: C9 Project</title>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA=Compatible" content="IE-edge,chrome=1" />
+        <meta name="robots" content="noindex,nofollow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <link rel="stylesheet" href="css/main.css" />
+    </head>
+    <body>
+        <div id="pageContainer">
+            <header>
+                
+            </header>
+            <main>
+                <div id="report" class="container">
+                    <h1 class="articleHeadline">HISTORY OF HELLO WORLD APP</h1>
+                    <p class="article">
+                        When beginning to enter the world of computer programming, coding for a rookie coder can seem 
+                        daunting. The lines of code don’t seem to make sense, and everything is like a foreign language, 
+                        because in fact it is, it is a new language that you are learning. So where does one usually start 
+                        the process and get your feet wet? It usually starts with beginning courses on how to code and with 
+                        a simple program of code entitled ‘Hello World.’ Hello World is the first line of code that beginner 
+                        coders learn. It is an easy line of code to show beginners how to get started, and is also used to 
+                        make sure the computer language is working correctly, as can be seen in this article from Wikipedia, 
+                        “A “Hello World” program is traditionally used to introduce novice programmers to a programming 
+                        language. “Hello, world!” is also traditionally used in a sanity test to make sure that a computer 
+                        language is correctly installed, and that the operator understands how to use it.” (Wikipedia)
+                    </p>
+                    <p class="article">
+                        Before ‘Hello World’ was introduced to the public, the thought of even working with computers seemed 
+                        unfathomable unless you were a scientist or someone who had a speciality occupation of working with 
+                        computers. There were no such things as personal home computers, and it really wasn’t a thought to even 
+                        own a computer in the average household as computers cost upwards of at least a million dollars and you 
+                        would have to be expertly trained in the world of computer science. You could get a degree in computer 
+                        science, but a certificate in computer programming did not exist as they do so today. With the introduction 
+                        of ‘Hello World’ it allowed computers more accessible to humans and began a better understanding of computers 
+                        to beginners and seasoned computer users as well, which can be seen in this article by Software Guild, “With 
+                        Hello World as a foundation, novice programmers can easily understand computer science principles or elements. 
+                        And professionals with years of coding experience can use it to learn how a given programming language works, 
+                        especially in terms of structure and syntax. With applications at all skill levels and in almost every language, 
+                        there is a long history behind such a short program.”(Thussong) 
+                    </p>
+                    <p class="article">
+                        The question that now remains is who actually came up with “Hello World”? Who was the genius who changed the way 
+                        we view and use computers forever? The person who came up with “Hello World” and brought it to life is believed 
+                        by many  from a man named Brian Kernighan through a book he had written, which can be seen in this article from 
+                        the Software Guild, “Though the origins of Hello World remain somewhat unclear, its use as a test phrase is widely 
+                        believed to have begun with Brian Kernigham’s 1972 book, A Tutorial Introduction to the Language B. In this text, 
+                        the first known version of the program was used to illustrate external variables. Because the previous example in 
+                        the tutorial printed “hi!” on the terminal, the more complex “hello, world!” required more character constants for 
+                        expression and was the next step in the learning process. From there, it was used in a Bell Laboratories memo in 
+                        1974, as well as The C Programming Language in 1978. This popular text is what made Hello World famous.” (Thussong)
+                    </p>
+                    <p class="article">
+                        ‘Hello World’ was the change that was needed to introduce computer programming to the masses. Brian Kernighan’s 
+                        tutorial has allowed all humans to have access to computer programming, and not just the few elites who knew how to 
+                        manage computers and their systems. This revolutionary phenomenon is ubiquitously widespread and is still used to 
+                        this day as an introductory test for beginning programmers. ‘Hello World’ is one statement that is used across multiple 
+                        computer languages, and has sustained a mark where it is not going anywhere anytime soon. 
+                    </p>
+                </div>
+                <div id="worksCited" class="container">
+                    <h2 class="citationHeading">Works Cited:</h2>
+                    <p class="citation">
+                        Trikha, Ritikha. “The History of Hello World” Hackerrank.com 21 April 2015 https://blog.hackerrank.com/the-history-of-hello-world/ 
+                    </p>
+                    <p class="citation">
+                        Wikipedia. “Hello World Program” Wikipedia.com np nd 02 March 2019.https://en.wikipedia.org/wiki/%22Hello,_World!%22_program 
+                    </p>
+                    <p class="citation">
+                        Thussong. “The History of Hello World” thesoftwareguild.com 17 July 2015 https://www.thesoftwareguild.com/blog/the-history-of-hello-world/  
+                    </p>
+                </div>
+                <div id="otherSrcs" class="container">
+                    <h2 class="citationHeading">Other Sources:</h2>
+                    <ul class="srcList">
+                        <li>
+                            <a href="https://ngoprex.com/history-of-hello-world-who-made-the-first-helloword/" target="_blank">
+                                History of Hello World -- Who Made the First Hello World?
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.quora.com/What-does-Hello-World-mean" target="_blank">
+                                Quora: What Does Hello World Mean?
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://blog.hackerrank.com/the-history-of-hello-world/" target="_blank">
+                                Hacker Rank: History of 'Hello, World'
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://stackoverflow.com/questions/602237/where-does-hello-world-come-from" target="_blank">
+                                Stack Overflow: Where Does Hello World Come From?
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </main>
+            <footer>
+                
+            </footer>
+        </div>
+    </body>
+</html>
